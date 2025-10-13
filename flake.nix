208e3f319e75797ccb9f66678895036696104fea
emanueljg <emanueljohnsongodin@gmail.com>
2025-10-13 13:57:49 +0200

    update garnix.yaml

--- flake.nix
+++ flake.nix
@@ -94,6 +94,27 @@
         ) self.nixosConfigurations
       ) systemPkgs;
 
+      apps = builtins.mapAttrs (system: pkgs: {
+        dockerPush = {
+          type = "app";
+          program = lib.getExe (
+            pkgs.writeShellApplication {
+              name = "dockerPush";
+              runtimeInputs = [ ];
+              text = ''
+                echo hello
+                echo $GARNIX_CI
+                echo $GARNIX_BRANCH
+                echo $GARNIX_COMMIT_SHA
+                echo $GARNIX_ACTION_PRIVATE_KEY_FILE
+                curl https://garnix.io/api/keys/emanueljg/config/dockerPush/key.public
+              '';
+
+            }
+          );
+        };
+      }) systemPkgs;
+
       nixosConfigurations =
         builtins.removeAttrs (builtins.mapAttrs (_: v: lib.nixosSystem v) self.configs)
           [
