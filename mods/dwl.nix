7a524a43b2e40c9f2234ae55d777048531ac2caf
emanueljg <emanueljohnsongodin@gmail.com>
2025-11-17 16:33:27 +0100

    typo

--- mods/dwl.nix
+++ mods/dwl.nix
@@ -8,10 +8,8 @@
         owner = "emanueljg";
         repo = "dwl";
         rev = "config";
-        hash = "sha256-y5V7ODC9CTIWEocyXpIwlcHHuuXYjWu025fWjHDRHoc=";
-        # hash = "sha256-hHeLQj7Pvxd23QvJM7OANIY3Q0I78CT3uaoCkmZN9Ik=";
-        # hash = "sha256-TgyCpx5GBoF5CNsrMijseRl/r3ghSlQj6XKw2XHqAe8=";
-        # hash = "sha256-Tjz24MXpcAx+JWL8obxOlhR8JhkZZsfA7BZ8ghwLRcw=";
+        hash = "sha256-NKNWeGN+Z/tWQ/WCnEdXFwimLNRdqKLoGNCTQnoUUlI=";
+        # hash = "sha256-NKNWeGN+Z/tWQ/WCnEdXFwimLNRdqKLoGNCTQnoUUlI=";
       };
     };
   };
