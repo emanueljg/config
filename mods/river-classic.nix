1924f20e00cf11c39c32e80e8ee9a469611672b3
emanueljg <emanueljohnsongodin@gmail.com>
2025-11-25 15:02:31 +0100

    fix border stuff

--- mods/river-classic.nix
+++ mods/river-classic.nix
@@ -1,4 +1,9 @@
-{ lib, pkgs, ... }:
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
 {
   imports = [
     ./local/river-classic
@@ -9,13 +14,26 @@
   local.river-classic = {
     enable = true;
     init = {
-      text = ''
-        riverctl keyboard-layout \
-          -variant 'altgr-intl' \
-        	-options 'caps:swapescape' \
-          us
-        riverctl map normal Alt Backspace spawn qutebrowser
-      '';
+      text =
+        let
+          theme = config.local.themes."Everforest Dark Medium";
+        in
+        ''
+          riverctl keyboard-layout \
+            -variant 'altgr-intl' \
+          	-options 'caps:swapescape' \
+            us
+
+          riverctl map normal Alt Backspace spawn qutebrowser
+
+          riverctl set-cursor-warp on-focus-change
+          riverctl hide-cursor when-typing enabled
+
+          riverctl border-width 5
+          riverctl border-color-focused 0x${lib.removePrefix "#" theme.fg.statusline1}
+
+          riverctl send-layout-cmd rivertile "main-ratio 0.55"
+        '';
     };
   };
 }
