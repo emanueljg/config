f89a5ca0799bae5cd6e47dd318f67de70336e722
Emanuel Johnson Godin <emanueljohnsongodin@gmail.com>
2026-01-06 15:45:14 +0100

    Update default.nix

--- mods/local/runapp/default.nix
+++ mods/local/runapp/default.nix
@@ -10,11 +10,7 @@ in
 {
   options.local.runapp = {
     enable = lib.mkEnableOption "runapp";
-    package = lib.mkOption {
-      type = lib.types.package;
-      # https://github.com/NixOS/nixpkgs/pull/447721
-      default = pkgs.callPackage ./package.nix { };
-    };
+    package = lib.mkPackageOption pkgs "runapp" { };
   };
 
   config = lib.mkIf cfg.enable {
